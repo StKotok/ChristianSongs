08f53cd87353ed83a1946c5fad1fae9add15cf9c
StKotok <stkotok@gmail.com>
2016-05-03 22:50:18 +0300

    model

--- app/src/main/java/co/neatapps/allchristsongs/android/model/Digest.java
+++ app/src/main/java/co/neatapps/allchristsongs/android/model/Digest.java
@@ -2,6 +2,15 @@ package co.neatapps.allchristsongs.android.model;
 
 public class Digest {
 
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     public enum DigestType {
         revivalSong,
--- app/src/main/java/co/neatapps/allchristsongs/android/model/Song.java
+++ app/src/main/java/co/neatapps/allchristsongs/android/model/Song.java
@@ -4,8 +4,32 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Song {
-    private Map<Digest.DigestType, Integer> number = new TreeMap<>();
+    private Map<Digest.DigestType, Integer> numbers = new TreeMap<>();
     private String header;
     private String body;
+
+    public Map<Digest.DigestType, Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(Map<Digest.DigestType, Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 
 }
